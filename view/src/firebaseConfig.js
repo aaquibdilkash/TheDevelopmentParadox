4a1cd9b00816792490b992b7313458642be9e500
Aaquib Dilkash <aaquibdilkash@gmail.com>
2020-12-26 21:50:19 +0530

    added firebaseConfig.js

---
+++ view/src/firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyAAoVV3KTPcL4IuoQwRxy6UR2ypxtAj9vE",
+    authDomain: "thedevelopmentparadox.firebaseapp.com",
+    projectId: "thedevelopmentparadox",
+    storageBucket: "thedevelopmentparadox.appspot.com",
+    messagingSenderId: "728503463094",
+    appId: "1:728503463094:web:454afd4d647e9c95d84a17",
+    measurementId: "G-KRJHDVG8Z5"
+  };
